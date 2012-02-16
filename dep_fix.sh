a184fdbcda1bfddb6ff64b606de99bc0d178be90
Roman Asendorf <roman.asendorf@gmail.com>
2012-02-16 16:09:07 +0100

    fixed - error handling

--- dep_fix.sh
+++ dep_fix.sh
@@ -2,20 +2,20 @@
 
 # Rebuild dependencies
 echo "Step 1/4: Fixing dependencies (revdep-rebuild)."
-revdep-rebuild || echo "Step 1/4: Error while fixing dependencies."; exit 1
+revdep-rebuild || (echo "Step 1/4: Error while fixing dependencies."; exit 1)
 echo "Step 1/4: Finished dependencies check/fixing."
 
 # fix la files
 echo "Step 2/4: Fixing lafiles (lafilefixer)."
-lafilefixer || echo "Step 2/4: Error while fixing lafiles."; exit 1
+lafilefixer || (echo "Step 2/4: Error while fixing lafiles."; exit 1)
 echo "Step 2/4: Finished lafile fixing."
 
 # fix python linkage
 echo "Step 3/4: Fixing python linkage (python-updater)."
-python-updater || echo "Step 3/4: Error while fixing python modules."; exit 1
+python-updater || (echo "Step 3/4: Error while fixing python modules."; exit 1)
 echo "Step 3/4: finished python linkage checking/fixing."
 
 # fix perl linkage
 echo "Step 4/4: Fxing perl linkage (perl-cleaner)."
-perl-cleaner --modules || echo "Step 4/4: Error while fixing perl modules."; exit 1
+perl-cleaner --modules || (echo "Step 4/4: Error while fixing perl modules."; exit 1)
 echo "Step 4/4: Finished perl linkage checking/ficing."
--- eup.sh
+++ eup.sh
@@ -7,7 +7,7 @@ args=$@
 echo "Running portage sync and update."
 echo ""
 echo "Starting sync..."
-eix-sync || echo "Sync failed. Canceling further actions."; exit 1
+eix-sync || (echo "Sync failed. Canceling further actions."; exit 1)
 
 echo "Sync completed. Starting update..."
 emerge -DuvN $args --with-bdeps=y @world || echo "Updated failed due to above error."
