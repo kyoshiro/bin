5648842525c7c6a7a2ea03a00d74a22bdfba83f4
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2020-02-05 08:56:26 +0100

    fixed comparison on toggle cpu state governor; added switch mechanism between powersave and performance

--- toggle_cpu_state.sh
+++ toggle_cpu_state.sh
@@ -6,12 +6,12 @@ mode="powersave"
 current_state="$(cpufreq-info -o | awk '{ print $14 }' | grep -m 1 powersave)"
 
 if [ -n "$current_state" ]; then
-	if [ "$current_state" == "powersave" ]; then
-		mode="ondemand"
-	fi
+	if [ "$current_state"=="powersave" ]; then
+		mode="performance"
+    fi
 fi
 
-for cpu in 0 1; do
+for cpu in 0 1 2 3 4 5 6 7; do
 	echo "Toggling state to $mode."
     cpufreq-set -g $mode -c $cpu
 done
