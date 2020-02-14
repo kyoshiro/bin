df97079a3471cd0ba5288a58289a145b65a4ed37
Roman Asendorf <roman.asendorf@gmail.com>
2020-02-14 13:17:45 +0100

    start screensaver early

--- lockscreen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LOCKSCREENIMG='/tmp/lockscreen.png'
-
-pgrep i3lock || ffmpeg  -loglevel panic -f x11grab -video_size $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/') -y -i :0.0+$1,20 -i ~/Pictures/tmnt_small.png -filter_complex "boxblur=9,overlay=(main_w-overlay_w)/3:(main_h-overlay_h)/2" -vframes 1 $LOCKSCREENIMG
-
-i3lock -i $LOCKSCREENIMG
