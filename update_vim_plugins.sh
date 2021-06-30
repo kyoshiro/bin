4b0341fcdbf40ab396c67016582a278297bd7b8a
Roman Asendorf <roman.asendorf@gmail.com>
2021-06-30 20:37:38 +0200

    switched vim updates to forked repos fetch; changed i3lock image reference; added xrandr layout scripts

--- update_vim_plugins.sh
+++ update_vim_plugins.sh
@@ -4,7 +4,7 @@ plugdir=~/dev/git/vimplugins
 
 for plug in `cat ~/dev/git/dotfiles/vim.plugins.urls`
 	do
-		plugname=$(echo $plug | sed -e 's,https://.*/\(.*\).git,\1,g')
+		plugname=$(echo $plug | sed -e 's,git@github.com:.*/\(.*\).git,\1,g')
 		echo "This plugin: $plugname";
 		echo "Plugin URL: $plug";
 		currplugdir=$plugdir/$plugname
