24c3f5017fb570ed0a1ec5fd0a48fe60a623483d
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2021-11-12 21:34:00 +0100

    updated backup scripts

--- backup_home.sh
+++ backup_home.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo tar -cjSvp --numeric-owner --atime-preserve -f /media/backup/`date +%Y%m%d_%H%M`-`hostname`-home.tar.bz2 /home
+sudo tar -cjSvp --numeric-owner --atime-preserve -X exclude_home_dirs -f /media/backup/`date +%Y%m%d_%H%M`-`hostname`-home.tar.bz2 /home
