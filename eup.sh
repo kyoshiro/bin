8365f0c87ede48c210e5b220c9a86140eb717d2d
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2025-06-18 15:46:02 +0200

    add: gentoo update timer and service

--- eup.sh
+++ eup.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
 # This script automates the update process
-# Don't forget to run revdep-rebuild, modules-rebuild, perl-cleaner
-# modules and python-updater
-args=$@
+#
+LOGFILE="/var/log/emerge-update.log"
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+USER_TO_NOTIFY="rasendorf"
 
-echo "Running portage sync and update."
-echo "Starting sync... skipping on yukimura"
 
-if ! [ `hostname` == "yukimura.iot.intern" ]; then
-	eix-sync || (echo "Sync failed. Canceling further actions."; exit 1)
-	echo "Sync completed..."
-fi
-echo "Starting update..."
-emerge -DuvN $args --with-bdeps=y @world || echo "Updated failed due to above error."
-echo "Cleaning unnecessary distfiles..."
-eclean-dist --deep --fetch-restricted
-echo "It's safe to run 'emerge --deplean -a' now."
-echo ""
-echo "Happy Gentoo'ing..."
+for user in $(who | awk '{print $1}' | sort -u); do
+    uid=$(id -u "$user")
+    export DISPLAY=:0
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$uid/bus"
+
+    sudo -u "$user" DISPLAY=$DISPLAY DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+    notify-send "🔄 Systemupdate started..."
+done
+
+{
+    args=$@
+
+    echo " --- $DATE ---"
+    echo "ℹ️ Running portage sync and update process..."
+    echo "🔄 Starting sync... skipping on yukimura"
+
+    if ! [ `hostname` == "yukimura.iot.intern" ]; then
+    	eix-sync || (echo "⚠️ Sync failed. Canceling further actions."; exit 1)
+    	echo "✅ Sync completed..."
+    fi
+    echo "🔄 Checking known vulnerabilities..."
+    glsa-check --list
+    echo "🔄 Starting update..."
+    emerge -DuvN $args --with-bdeps=y @world || echo "❌ Updated failed due to above error."
+    echo "📦 Running emerge @preserved-rebuild..."
+    emerge @preserved-rebuild
+    echo "🧹 Cleaning unnecessary distfiles..."
+    eclean-dist --deep --fetch-restricted
+    echo "✅ Updates and cleanups completed."
+    echo ""
+    echo "💡 It's safe to run 'emerge --depclean -a' now."
+    echo ""
+    echo "🍻 Happy 🐧 Gentoo'ing..."
+    echo " --- $DATE ---"
+} >> "$LOGFILE" 2>&1
+
+for user in $(who | awk '{print $1}' | sort -u); do
+    uid=$(id -u "$user")
+    export DISPLAY=:0
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$uid/bus"
+
+    sudo -u "$user" DISPLAY=$DISPLAY DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+    notify-send "✅ Systemupdate finished, for more info, see log file $LOGFILE."
+
+done
+
