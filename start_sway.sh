61f568bdd1bd5394eb25f5729c174e4d4926dee1
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2026-02-20 15:47:09 +0100

    add gentoo update openrc scripts; fix sway start

--- start_sway.sh
+++ start_sway.sh
@@ -5,10 +5,10 @@ export QT_QPA_PLATFORM=wayland-egl
 
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 
-export XDG_RUNTIME_DIR=/run/user/`id -u`
+export XDG_RUNTIME_DIR=/var/run/user/`id -u`
 export XDG_SESSION_TYPE="wayland"
 export XDG_SESSION_DESKTOP="sway:wlroots"
 export XDG_CURRENT_DESKTOP="sway:wlroots"
 export DESKTOP_SESSION="sway"
 
-exec dbus-run-session /usr/bin/sway
+exec dbus-run-session /usr/local/bin/sway
