eb07532ee4cadd733b4ad0d6429c14025ca252b2
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2026-02-28 17:34:24 +0100

    update start and lock scriptings

--- start_sway.sh
+++ start_sway.sh
@@ -11,4 +11,18 @@ export XDG_SESSION_DESKTOP="sway:wlroots"
 export XDG_CURRENT_DESKTOP="sway:wlroots"
 export DESKTOP_SESSION="sway"
 
-exec dbus-run-session /usr/local/bin/sway
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+  platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+  platform='freebsd'
+fi
+
+if [[ "$platform" == "freebsd" ]]; then
+  # Check if we are on FreeBSD
+  exec dbus-launch --exit-with-session /usr/local/bin/sway
+else
+  # Else, we assume we are on Linux
+  exec dbus-launch --exit-with-session /usr/bin/sway
+fi
